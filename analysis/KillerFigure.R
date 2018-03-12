8fe018230bdb46f8b4a9d1f39e2e1caf5b0ffe11
pdolder <paul.dolder@gmail.com>
2018-03-12 20:38:08 +0000

    What went wrong!

--- analysis/KillerFigure.R
+++ analysis/KillerFigure.R
@@ -12,17 +12,37 @@ library(reshape2)
 
 ## Repurpose the aggregation logs bit of close_areas
 
-get_data <- function(basis = NULL, yr = NULL, mn = NULL, wk = NULL, dataIn = NULL, sim_init = NULL) {
+get_data <- function(basis = NULL, yr = NULL, mn = NULL, wk = NULL, aggBasis = 'None', dataIn = NULL, sim_init = NULL) {
 
 ## For commercial data
 if(basis == 'commercial') {
 
 		## Get data
 		logs <- as.data.frame(combine_logs(dataIn))
-	
+		
+		if(aggBasis == 'None') {
 		interpdat <- filter(logs, year %in% yr, month %in% mn, week %in% wk) %>% 
 			group_by(x, y) %>% 
 			summarise(spp_1 = sum(spp1), spp_2 = sum(spp2), spp_3 = sum(spp3), spp_4 = sum(spp4))
+		}
+
+		if(aggBasis == 'year') {
+		interpdat <- filter(logs, year %in% yr, month %in% mn, week %in% wk) %>% 
+			group_by(x, y, year) %>% 
+			summarise(spp_1 = sum(spp1), spp_2 = sum(spp2), spp_3 = sum(spp3), spp_4 = sum(spp4))
+		}
+
+		if(aggBasis == 'month') {
+		interpdat <- filter(logs, year %in% yr, month %in% mn, week %in% wk) %>% 
+			group_by(x, y, year, month) %>% 
+			summarise(spp_1 = sum(spp1), spp_2 = sum(spp2), spp_3 = sum(spp3), spp_4 = sum(spp4))
+		}
+		
+		if(aggBasis == 'week') {
+		interpdat <- filter(logs, year %in% yr, month %in% mn, week %in% wk) %>% 
+			group_by(x, y, year, month, week) %>% 
+			summarise(spp_1 = sum(spp1), spp_2 = sum(spp2), spp_3 = sum(spp3), spp_4 = sum(spp4))
+		}
 
 		} ## end commercial basis
 			
@@ -30,8 +50,16 @@ if(basis == 'commercial') {
 if(basis == 'survey') {
 		logs <- as.data.frame(dataIn)
 		## Filter as appropriate:- here based on either yr_base or
+
+		if(aggBasis == 'None') {
 		interpdat <- filter(logs, year %in% yr) %>% group_by(x, y) %>% 
 	   	summarise(spp_1 = sum(spp1), spp_2 = sum(spp2), spp_3 = sum(spp3), spp_4 = sum(spp4))
+		}
+
+		if(aggBasis == 'year') {
+		interpdat <- filter(logs, year %in% yr) %>% group_by(x, y, year) %>% 
+	   	summarise(spp_1 = sum(spp1), spp_2 = sum(spp2), spp_3 = sum(spp3), spp_4 = sum(spp4))
+		}
 
 		} ## End survey basis 
 
@@ -53,7 +81,7 @@ if(basis == 'real_pop') {
 			   y = rep(seq_len(100), each = 100),
 			   year = y, month = unique(sim_init[["brk.idx"]][["month.breaks"]][sim_init[["brk.idx"]][["week.breaks"]]==w]),
 			   week = w,  do.call(cbind,res2)))
-
+)
 		return(res3)
 		})
 
@@ -64,8 +92,29 @@ if(basis == 'real_pop') {
 		logs <- do.call(rbind, logs_yr_list)
 
 		## Filter as appropriate:- here based on either yr_base or
+
+		if(aggBasis == 'None') {
 		interpdat <- filter(logs, year %in% yr) %>% group_by(x, y) %>% 
 		summarise(spp_1 = sum(spp1), spp_2 = sum(spp2), spp_3 = sum(spp3), spp_4 = sum(spp4))
+
+		}
+	
+		if(aggBasis == 'year') {
+		interpdat <- filter(logs, year %in% yr) %>% group_by(x, y, year) %>% 
+		summarise(spp_1 = sum(spp1), spp_2 = sum(spp2), spp_3 = sum(spp3), spp_4 = sum(spp4))
+
+		}
+
+		if(aggBasis == 'month') {
+		interpdat <- filter(logs, year %in% yr) %>% group_by(x, y, year, month) %>% 
+		summarise(spp_1 = sum(spp1), spp_2 = sum(spp2), spp_3 = sum(spp3), spp_4 = sum(spp4))
+		}
+
+		if(aggBasis == 'week') {
+		interpdat <- filter(logs, year %in% yr) %>% group_by(x, y, year, month, week) %>% 
+		summarise(spp_1 = sum(spp1), spp_2 = sum(spp2), spp_3 = sum(spp3), spp_4 = sum(spp4))
+		}
+
 
 } ## End real pop basis
 
@@ -108,7 +157,7 @@ cols <- c("red", "blue", "purple", "green")
 #par(mar = c(0,0,0,0), pty = 'm')
 basemap(xlim = c(0,100), ylim = c(0,100), xlab = "", ylab = "", bg = "white", xaxt = 'n', yaxt = "n")
 draw.barplot2D(catch_comp[[1]]$x,catch_comp[[1]]$y,z = catch_comp[[1]]$z, width = gran,
-	       height = gran, xlab = "", ylab = "", col = cols, lwd.frame = 0.3, col.frame = "white")
+	       height = gran, xlab = "", ylab = "", col = cols, lwd.frame = 0.1, col.frame = "white")
 
 
 }
@@ -141,8 +190,9 @@ plot_comp(gran = 1, dataIn = dataIn)
 
 ##############################################
 ## Setting out the plot
+setEPS()
+postscript('Data_Aggregation.eps', width = 8 * 2, height = 8 * 3, bg = "white")
 
-png("Data_Aggregation.png", width = 600 * 2, height = 600 * 3)
 par(oma = c(2,2,12,12), mar = c(0,0,0,0), mfrow = c(4,3))
 
 ## real pop 
@@ -184,15 +234,77 @@ dev.off()
 
 ## Some measure of temporal change in distribtions
 
+catch_comp <- function (basis = NULL, dataIn = NULL, timestep = NULL, xs = NULL, ys = NULL) {
+
+	if(timestep == "annual") {
+catch_comp <- matrix(NA, nc = 1 * 3, nr = 4)
+	}
+	if(timestep == "monthly") {
+catch_comp <- matrix(NA, nc = 12 * 3, nr = 4)
+	}
+	if(timestep == "weekly") {
+catch_comp <- matrix(NA, nc = 52 * 3, nr = 4)
+	}
 
 
-catch_comp <- matrix(NA, nc = 52, nr = 4)
+	dataPlot <- filter(dataIn, x %in% xs, y %in% ys) %>%
+		group_by(year, month, week) %>%
+		summarise(spp_1 = sum(spp_1),
+			  spp_2 = sum(spp_2),
+			  spp_3 = sum(spp_3),
+			  spp_4 = sum(spp_4))
+
+
+
+	if(basis == 'commercial') {
 
 for(i in 1:4) { ## spp
+
+	if(timestep == 'weekly')
 	for(j in 1:52) {  ##wk
-		
     catch_comp[i,j] <- B / allB 	}
 }
+
+	}
+
+
+
+
+
+	
+}
+
+
+dataIn <- get_data(basis = 'commercial', yr = yr, mn = mn, wk = wk, aggBasis = 'month',dataIn = res[["fleets_catches"]]) %>%
+	filter(x %in% c(40:50), y %in% 20:30) %>% group_by(year, month) %>% summarise(spp_1 = sum(spp_1), spp_2 = sum(spp_2), spp_3 = sum(spp_3), spp_4 = sum(spp_4))
+
+dataIn <- dataIn[order(dataIn$year, dataIn$month),]
+dataIn[,3:6] <- dataIn[,3:6] / rowSums(dataIn[,3:6])
+
+dataPlot <- expand.grid(year = 2:4, month = 1:12, spp_1 = NA, spp_2 = NA, spp_3 = NA, spp_4 = NA)
+
+dataPlot$spp_1 <- dataIn$spp_1[match(paste(dataPlot$year, dataPlot$month),
+				     paste(dataIn$year, dataIn$month))]
+dataPlot$spp_2 <- dataIn$spp_2[match(paste(dataPlot$year, dataPlot$month),
+				     paste(dataIn$year, dataIn$month))]
+dataPlot$spp_3 <- dataIn$spp_3[match(paste(dataPlot$year, dataPlot$month),
+				     paste(dataIn$year, dataIn$month))]
+dataPlot$spp_4 <- dataIn$spp_4[match(paste(dataPlot$year, dataPlot$month),
+				     paste(dataIn$year, dataIn$month))]
+
+dataPlot[,3:6][is.na(dataPlot[,3:6])] <- 0
+dataPlot <- dataPlot[order(dataPlot$year, dataPlot$month),]
+
+
+cols <- c("red", "blue", "purple", "green")
+
+barplot(t(dataPlot[,3:6]), names.arg = rep(paste(""), 36), col = cols)
+
+par(oma = c(8,2,2,2), mar = c(0,0,0,0), mfrow = c(1,1))
+barplot(t(dataPlot[,3:6]),names.arg = dataPlot$month, col = cols)
+mtext(text = "2                         3                    4", side = 1, line = 2, outer = T, font = 2,   cex = 1 ) ## top
+
+
 
 png(file.path("..","tests" , "plots", "Proportion_in_cell.png"), width = 800, height = 800)
 matplot(t(catch_comp), type = "l", ylab = "Proportion of population in cell")
